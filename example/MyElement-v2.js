b76149ab6ddbc3bc25e005541eb4302649ef4139
Artur Parkhisenko <ikeagold@gmail.com>
2016-11-20 23:12:49 +0200

    :fireworks: initial upload

---
+++ example/MyElement-v2.js
@@ -0,0 +1,49 @@
+// Extend Polymer.Element base class
+class MyElement extends Polymer.Element {
+
+  static get is() {
+    return 'my-element';
+  }
+
+  static get config() {
+    return {
+      /* properties, observers meta data */
+      properties: {
+        foo: {
+          type: String,
+          value: 'hello'
+        }
+      }
+    }
+  }
+
+  // constructor() {
+  //   super();
+  //
+  //   console.log('created');
+  // }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    this.textContent = this.foo;
+
+    console.log('attached', this.foo);
+
+    console.log('props:');
+    for (var k in this) {
+      if (this.hasOwnProperty(k)) {
+        console.log(k);
+      }
+    }
+  }
+
+  // ready() {
+  //   console.log('ready');
+  // }
+
+  //...
+}
+
+// Register custom element definition using standard platform API
+customElements.define(MyElement.is, MyElement);
